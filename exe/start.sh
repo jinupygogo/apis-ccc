c02dc84549430ff8287dcf723bfbb790cf6416a2
岩瀬俊一郎 <iwaseshunichirou@iwaseshunichirounoMacBook-puro.local>
2020-10-27 10:07:37 +0900

    Change many files.

---
+++ exe/start.sh
@@ -0,0 +1,10 @@
+echo "call start.sh"
+
+UNAME=$(uname)
+if [ "$UNAME" = 'Darwin' ] ; then
+	java -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Tokyo -Djava.util.logging.config.file=./logging.properties -Dvertx.hazelcast.config=./cluster-mac.xml -jar ../target/apis-ccc-3.0.0-fat.jar -conf ./config.json -cluster -cluster-host 127.0.0.1
+else
+	java -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Tokyo -Djava.util.logging.config.file=./logging.properties -Dvertx.hazelcast.config=./cluster.xml -jar ../target/apis-ccc-3.0.0-fat.jar -conf ./config.json -cluster -cluster-host 127.0.0.1
+fi
+
+echo "... done"
